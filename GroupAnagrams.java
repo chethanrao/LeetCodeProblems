0fc09e1a75ed88c9019b530c239f5fe5f9f4b460
Chethan Rao <chethanrao@Chethans-MacBook-Pro.local>
2017-11-30 20:57:20 -0500

    Fixed some obvious bugs

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -12,57 +12,69 @@ Return:
 ]
 Note: All inputs will be in lower-case.
 */
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public static List<List<String>> groupAnagrams(String[] strs) {
       
-        HashMap<String,List<String>> hashMap=new HashMap<String,List<String>>();
+        HashMap<Double,List<String>> hashMap=new HashMap<Double,List<String>>();
         
         List<Integer> list=getPrimes();
         
-        System.out.println(list);
         
         HashMap<Character,Integer> alphaHashMap=getAlphabetPrimeHashMap(list);
-                System.out.println(alphaHashMap);
 
         
         for (String s:strs){
-            int product=getProductOfPrimes(alphaHashMap,s);
+            double product=getProductOfPrimes(alphaHashMap,s.toLowerCase());
+            
             
             if (hashMap.containsKey(product)){
                 List<String> existingList=hashMap.get(product);
                 existingList.add(s);
+                hashMap.put(product,existingList);
             }
             else{
                 List<String> newlist=new ArrayList<String>();
                 newlist.add(s);
-                hashMap.put(s,newlist);
+                hashMap.put(product,newlist);
             }
         
-            
         }   
         List<List<String>> finallist=new ArrayList<List<String>>();
         for (List<String> anagramlist:hashMap.values()){
+          
+            
             finallist.add(anagramlist);
+            
         }
         
         return finallist;
     }
     
-    public int getProductOfPrimes(HashMap<Character,Integer> hashMap,String str){
-        int product=1;
+    public static double getProductOfPrimes(HashMap<Character,Integer> hashMap,String str){
+        double product=1.0;
         for (int i=0;i<str.length();i++){
             product=product*hashMap.get(str.charAt(i));
         }
+        
+          System.out.println(product);
+                    System.out.println(str);
+                              System.out.println("---------------");
+
+
+      
         return product;
         
     }
     
-    public HashMap<Character,Integer> getAlphabetPrimeHashMap(List<Integer> list){
+    public static HashMap<Character,Integer> getAlphabetPrimeHashMap(List<Integer> list){
         int start=(int)'a';
         HashMap<Character,Integer> hashMap=new HashMap<Character,Integer>();
         
-        for (int i=0;i<25;i++){
+        for (int i=0;i<26;i++){
             hashMap.put((char)start,list.get(i));
             start=start+1;      
         }
@@ -71,33 +83,38 @@ class Solution {
         
     }
     
-    public List<Integer> getPrimes(){
+    public static List<Integer> getPrimes(){
     List<Integer> list=new ArrayList<Integer>();
-
+          list.add(2);
         int countOfPrimes=0,num=3;
         while (true){
             if (verifyPrime(num)){
-                countOfPrimes=countOfPrimes+1;
+                countOfPrimes++;
                 list.add(num);
-                num=num+2;
                 if (countOfPrimes==25)
                     break;
             }
+            num=num+2;
             
             
         }
         return list;
     }
     
-    public boolean verifyPrime(int num){
+    public static boolean verifyPrime(int num){
+    	
         
-        for (int i=3;i<Math.sqrt(num);i=i+1){
+        for (int i=2;i<Math.sqrt(num);i=i+1){
+        
+
             if (num%i==0)
                 return false;
         }
+
         return true;
     }
     
     
+  
     
 }

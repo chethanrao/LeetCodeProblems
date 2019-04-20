8d3b9078504e7c5eab18c0f132d67ec115e28b54
Chethan Rao <meetchethan@gmail.com>
2019-04-19 20:05:58 -0400

    Create NthDigit.java

---
+++ NthDigit.java
@@ -0,0 +1,77 @@
+/*
+Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
+
+Note:
+n is positive and will fit within the range of a 32-bit signed integer (n < 231).
+*/
+
+class Solution {
+    public int findNthDigit(int n) {
+	int countOfDigits=0,prevCountOfDigits=-1;
+	int numberOfDigits=1;
+	int totalNumberOfNumbersInTheRange=9;
+
+
+	while (countOfDigits<n){
+                System.out.println("countOfDigits:"+countOfDigits);
+                System.out.println("value of n:"+n);
+
+		prevCountOfDigits=countOfDigits;
+		countOfDigits=countOfDigits+numberOfDigits*totalNumberOfNumbersInTheRange;
+
+        totalNumberOfNumbersInTheRange=totalNumberOfNumbersInTheRange*10;
+        
+      
+        
+        numberOfDigits=numberOfDigits+1;
+        System.out.println("numberOfDigits:"+numberOfDigits);
+                System.out.println("totalNumberOfNumbersInTheRange:"+totalNumberOfNumbersInTheRange);
+                System.out.println("countOfDigits:"+countOfDigits);
+          if ((long)totalNumberOfNumbersInTheRange*10>=Integer.MAX_VALUE){
+              numberOfDigits=numberOfDigits+1;
+
+            break;
+        }
+
+	}
+        
+    --numberOfDigits;
+
+	int possibleNumber=getNumberFromDigits(numberOfDigits-1)+(n-prevCountOfDigits)/(numberOfDigits);
+            
+        
+        System.out.println("numberOfDigits:"+numberOfDigits);
+
+        System.out.println("possibleNumber:"+possibleNumber);
+                        System.out.println("prevCountOfDigits:"+prevCountOfDigits);
+
+                System.out.println("(n-prevCountOfDigits)%numberOfDigits:"+((n-prevCountOfDigits)%numberOfDigits));
+
+        
+	if ((n-prevCountOfDigits)%numberOfDigits==0)
+		return Character.getNumericValue(String.valueOf(possibleNumber).charAt(String.valueOf(possibleNumber).length()-1));
+	else{    
+       
+
+		return Character.getNumericValue(String.valueOf(possibleNumber+1).charAt((n-prevCountOfDigits)%(numberOfDigits)-1));
+    }
+
+	
+  }
+
+ public int getNumberFromDigits(int numberOfDigits){
+     if (numberOfDigits==0){
+         return numberOfDigits;
+     }
+     
+      
+	int num=9;
+     int base=9;
+	for (int i=1;i<numberOfDigits;i++){
+		num=num*10;
+        base=base+num;
+	}
+	return base;
+}
+
+} 
